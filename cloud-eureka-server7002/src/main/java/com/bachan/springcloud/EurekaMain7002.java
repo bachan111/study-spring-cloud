481e4896f926bff6ce7d7b9bd26ba4a166e1b9c5
常保峰 <changbaofeng@travelsky.com.cn>
2021-11-12 14:08:57 +0800

    eureka集群环境搭建

---
+++ cloud-eureka-server7002/src/main/java/com/bachan/springcloud/EurekaMain7002.java
@@ -0,0 +1,19 @@
+package com.bachan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author bachan
+ * @desc
+ * @date 2021/11/12
+ */
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
